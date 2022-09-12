b2ece4728372308efca4d20e9e4b72988a178d83
Pavel <holms001@mail.ru>
2022-09-12 14:12:30 +0300

    comment

--- Веб/web/includes/signupnew.inc.php
+++ Веб/web/includes/signupnew.inc.php
@@ -2,4 +2,5 @@
 require_once 'function.inc.php';
 require_once 'dbpdoconnection.inc.php';
 createUser($pdo, $_GET['email'], password_hash($_GET['pwd'], PASSWORD_DEFAULT));
+//createUser($pdo, $_GET['email'],$_GET['pwd']);
 ?>
